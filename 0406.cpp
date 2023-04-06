921722a037f203d8b03dd1e42b278b3840fd0581
Jelly <pbp980822@nate.com>
2023-04-06 09:28:32 +0900

    Game Class

---
+++ 0406.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+class Gmae
+{
+public:
+	int HP, MP;
+	float Attack;
+
+	int Player()
+	{
+
+	}
+	int Monster()
+	{
+		int Slime, Goblin, Wildboar;
+	}
+
+};
