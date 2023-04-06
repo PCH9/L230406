bb050da7bb3e0dc2facc727b0c5258ff7955bc54
Jelly <pbp980822@nate.com>
2023-04-06 10:12:52 +0900

    Plus Moove

--- 0406.cpp
+++ 0406.cpp
@@ -5,27 +5,44 @@ using namespace std;
 class Player
 {
 public:
-	int HP, MP;
-	float Attack;
+	int PlayerHP = 500;
+	int PlayerMP = 500;
+	int PlayerMove;
+	void AttackPlayer(float AttackPoint)
+	{
+		
+	}
+
+	void MovePlayer()
+	{
+		
+	}
 };
 
 class Monster
 {
 public:
-	int HP, MP;
-	float Attack;
+	int MonsterHP;
+	int MonsterMP;
+	int MonsterMove;
+	float AttackMonstar;
 	
 	int Slime()
 	{
-		
+		MonsterHP = 50;
+		MonsterMP = 50;
+		AttackMonstar = 10;
 	}
 	int Goblin()
 	{
-		
+		MonsterHP = 100;
+		MonsterMP = 100;
+		AttackMonstar = 50;
 	}
 	int Wildboar()
 	{
-		
+		MonsterHP = 150;
+		MonsterMP = 100;
+		AttackMonstar = 100;
 	}
-
 };
